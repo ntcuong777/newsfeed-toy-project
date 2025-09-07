65b2b650b1b9d582d86d5229795de7354dd6e617
Cuong Nguyen (Chris) <ntcuong777@gmail.com>
2025-09-07 23:09:18 +0700

    update small changes
    mostly focusing on learning the inner workings of libs this week

--- core/clj-web-dev/guestbook/project.clj
+++ core/clj-web-dev/guestbook/project.clj
@@ -30,17 +30,36 @@
                  [ring-webjars "0.2.0"]
                  [ring/ring-core "1.9.6"]
                  [ring/ring-defaults "0.3.4"]
-                 [selmer "1.12.55"]]
+                 [selmer "1.12.55"]
+                 [org.clojure/clojurescript "1.10.764" :scope "provided"]]
 
   :min-lein-version "2.0.0"
 
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
-  :resource-paths ["resources"]
+  :resource-paths ["resources" "target/cljsbuild"]
   :target-path "target/%s/"
   :main ^:skip-aot guestbook.core
 
-  :plugins [[cider/cider-nrepl "0.57.0"]]
+  :plugins [[cider/cider-nrepl "0.57.0"]
+            [lein-cljsbuild "1.1.8"]]
+
+  :cljsbuild
+  {:builds
+   {:app {:source-paths ["src/cljs"]
+          :compiler {:output-to "taret/cljsbuild/public/js/app.js"
+                     :output-dir "target/cljsbuild/public/js/out"
+                     :main "guestbook.core"
+                     :asset-path "/js/out"
+                     :optimizations :none
+                     :source-map true
+                     :pretty-print true}}}}
+
+  :clean-targets
+  ^{:protect false}
+  [:target-path
+   [:cljsbuild :builds :app :compiler :output-dir]
+   [:cljsbuild :builds :app :compiler :output-to]]
 
   :profiles
   {:uberjar {:omit-source true
